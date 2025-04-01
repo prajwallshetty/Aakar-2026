8c20749ba0efd3544d6d44b1f4f5de27cbd992c7
krithi <krithisonu.a.s@gmail.com>
2025-04-01 21:27:32 +0530

    adder register section to page

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,12 +1,14 @@
 import LandingPage from "@/components/(User)/Home/landingPage";
 import Image from "next/image";
 import RegisterSection from "@/components/(User)/Home/registerSection";
+import Footer from "@/components/(User)/footer";
 
 export default function Home() {
   return (
     <div className="h-[1000vh] font-GameOfSquids">
       <LandingPage />
       <RegisterSection />
+      <Footer />
     </div>
   );
 }
