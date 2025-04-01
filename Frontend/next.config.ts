6befb340d3ba96cd9fc100f80092225823f19046
paigauresh <paigauresh@gmail.com>
2025-04-01 21:01:39 +0530

    font fix

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(ttf|woff|woff2|eot|otf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "static/fonts/",
+          publicPath: "/_next/static/fonts/",
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
