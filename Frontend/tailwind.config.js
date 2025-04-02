198245df0d845c61e30b21f6eb7be13f15621e57
Keerthana430 <keerthanakrisuj@gmail.com>
2025-04-02 13:41:44 +0530

    tailwind config comment removed

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,18 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         gamesquid: ["GameOfSquids", "sans-serif"],
-//         inter: ["Inter", "sans-serif"],
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-
-
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
