bcac63b949a080e1ea876885f3c20aed8cd9f4be
jtuluve <jtuluve@gmail.com>
2025-04-05 11:21:04 +0530

    Added admin methods

---
+++ Frontend/backend/admin.ts
@@ -0,0 +1,42 @@
+import { Prisma } from "@prisma/client";
+import db from "./db";
+
+//Todo: need auth to complete this function
+export async function isAdmin() {
+
+}
+
+export async function getAdmins() {
+    return await db.admin.findMany()
+}
+
+export async function getAdmin(id: number) {
+    return await db.admin.findUnique({
+        where: {
+            id: id
+        }
+    })
+}
+
+export async function createAdmin(admin: Prisma.AdminCreateInput) {
+    return await db.admin.create({
+        data: admin
+    })
+}
+
+export async function deleteAdmin(id: number) {
+    return await db.admin.delete({
+        where: {
+            id: id
+        }
+    })
+}
+
+export async function updateAdmin(id: number, admin: Prisma.AdminUpdateInput) {
+    return await db.admin.update({
+        where: {
+            id: id
+        },
+        data: admin
+    })
+}
