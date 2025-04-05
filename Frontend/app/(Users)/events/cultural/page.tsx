3435783233fa7d1c6af1ccbc93e85a08db551cad
paigauresh <paigauresh@gmail.com>
2025-04-05 18:59:16 +0530

    different pages for events

---
+++ Frontend/app/(Users)/events/cultural/page.tsx
@@ -0,0 +1,12 @@
+import Eventpage from '@/components/(User)/events/eventpage'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <Eventpage />
+        </div>
+    )
+}
+
+export default page
