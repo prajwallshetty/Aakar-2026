8ee960d66b5fbb9aab28221742513497a838cdb0
paigauresh <paigauresh@gmail.com>
2025-04-05 17:41:07 +0530

    Register now component styling fixes

--- Frontend/components/(User)/Home/registerSection.tsx
+++ Frontend/components/(User)/Home/registerSection.tsx
@@ -1,23 +1,39 @@
 import { Inter } from 'next/font/google'
 import React from 'react'
 import Link from 'next/link'
-const inter = Inter({ subsets: ["latin"], weight: "400" })
+import Image from 'next/image'
+
+const inter = Inter({ subsets: ['latin'], weight: '400' })
+
 const RegisterSection = () => {
     return (
-        <section className="flex items-center justify-center text-white px-6 py-12 relative">
-            
-            <div className="text-center "> <h2 className="text-3xl md:text-5xl font-light pb-12">
-                Register Now</h2>
-                <p className={"text-2xl py-8 font-[Inter] line-height:100% letter-spacing:0% " + inter.className}> Don't miss out on the ultimate techno-cultural experience!
-                    <br /> Register now and be part of the excitement at <img className='w-30 h-auto inline-block ml-2' src="Aakarlogo.svg" alt="Logo" />
+        <section className="flex items-center justify-center text-white px-6 py-16 relative">
+            <div className="text-center max-w-3xl">
+                <h2 className="text-3xl md:text-5xl font-light pb-6">Register Now</h2>
+
+                <p className={`text-xl md:text-2xl py-6 ${inter.className}`}>
+                    Don’t miss out on the ultimate techno-cultural experience!
+                    <br />
+                    Register now and be part of the excitement at
+                    <span className="inline-block align-middle ml-2">
+                        <Image
+                            className="w-[100px] h-auto inline-block"
+                            src="/Aakarlogo.svg"
+                            width={100}
+                            height={100}
+                            alt="Aakar Logo"
+                        />
+                    </span>
                 </p>
+
                 <Link href="/register">
-                <button className='bg-[#AA1F26] hover:bg-red-700 text-white font-[Inter] font-light py-4 px-6 rounded-full text-xl leading-none'>Register Now</button>
+                    <button className="mt-6 bg-[#AA1F26] hover:bg-red-700 cursor-pointer transition duration-300 text-white font-light py-4 px-8 rounded-full text-xl leading-none">
+                        Register Now
+                    </button>
                 </Link>
             </div>
-        </section >
+        </section>
     )
 }
 
 export default RegisterSection
-
