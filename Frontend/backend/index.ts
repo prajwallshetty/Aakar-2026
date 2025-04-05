b7762a5ee0e1e58cb63bfb20d34b6099324ca3ef
jtuluve <jtuluve@gmail.com>
2025-04-05 19:37:48 +0530

    Added supabase, razorpay

--- Frontend/backend/index.ts
+++ Frontend/backend/index.ts
@@ -1,3 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import Razorpay from "razorpay";
+import { PrismaClient } from '@prisma/client';
+import { createClient } from "@supabase/supabase-js";
 
 export const db = new PrismaClient()
+
+export const razor = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_KEY!);
