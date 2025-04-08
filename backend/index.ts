7ea2368073ba5cbaed0e534577b1c44df6e62c87
jtuluve <jtuluve@gmail.com>
2025-04-08 14:20:55 +0530

    Removed razorpay

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,3 @@
-import Razorpay from "razorpay";
 import { PrismaClient } from '@prisma/client';
 
 export const db = new PrismaClient()
-
-export const razor = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
