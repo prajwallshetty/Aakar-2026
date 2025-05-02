b229db7113a092683f694ac5e5f19e3ea154ef3a
paigauresh <paigauresh@gmail.com>
2025-05-02 15:42:57 +0530

    postponement notice modal added

--- app/(Users)/page.tsx
+++ app/(Users)/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import GlimpseOfAakar from "@/components/(User)/Home/glimpseofaakar";
 import LandingPage from "@/components/(User)/Home/landingPage";
 import RegisterSection from "@/components/(User)/Home/registerSection";
@@ -7,11 +9,21 @@ import Concert from "@/components/(User)/Home/Concert";
 import ArenaBanner from "@/components/(User)/Home/ArenaBanner";
 import Sponsors from "@/components/(User)/Home/Sponsors";
 import Loading from "@/components/loading";
+import Notice from "@/components/notice";
+import { useState } from "react";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(true);
   return (
     <>
       <Loading />
+      {showModal && (
+        <Notice
+          title="Notice"
+          description="Due to present circumstances AAKAR 2025 is postponed. The dates will be announced soon. STAY TUNED!!"
+          onClose={() => setShowModal(false)}
+        />
+      )}
       <LandingPage />
       <Eventcards />
       <GlimpseOfAakar />
