860900095592c2791adcf5acb3346562ba5606b7
paigauresh <paigauresh@gmail.com>
2025-05-19 23:59:54 +0530

    Notice removed

--- app/(Users)/page.tsx
+++ app/(Users)/page.tsx
@@ -5,14 +5,14 @@ import Eventcards from "@/components/(User)/Home/Eventcards";
 import DJ from "@/components/(User)/Home/DJ";
 import Concert from "@/components/(User)/Home/Concert";
 import ArenaBanner from "@/components/(User)/Home/ArenaBanner";
-import Sponsors from "@/components/(User)/Home/Sponsors";
+// import Sponsors from "@/components/(User)/Home/Sponsors";
 import Loading from "@/components/loading";
-import Notice from "@/components/notice";
+// import Notice from "@/components/notice";
 
 export default function Home() {
   return (
     <>
-      <Notice title="🎮 Limited-Time Offer: Gaming Event Fees Slashed! 🚀" description="Ready to game? Now’s your chance! For a short time only, registration fees for all gaming events have been reduced to boost the thrill! Don’t miss out – register now and join the battle. Offer ends soon!" />
+      {/* <Notice title="🎮 Limited-Time Offer: Gaming Event Fees Slashed! 🚀" description="Ready to game? Now’s your chance! For a short time only, registration fees for all gaming events have been reduced to boost the thrill! Don’t miss out – register now and join the battle. Offer ends soon!" /> */}
       <Loading />
       <LandingPage />
       <Eventcards />
