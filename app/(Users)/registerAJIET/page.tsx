825b644dd8e9920cee0146285f3ee5a0bf8cf633
paigauresh <paigauresh@gmail.com>
2025-05-23 02:56:04 +0530

    closed registrations for all the events

--- app/(Users)/registerAJIET/page.tsx
+++ app/(Users)/registerAJIET/page.tsx
@@ -68,7 +68,7 @@ const Register = () => {
                 const events = await getAllEvents()
                 setEvents(events)
 
-                const eventsToFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
+                const eventsToFilter = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41 ];
                 setFilteredEventIds(eventsToFilter)
 
                 const filteredOptions = eventOptions.map((category) => ({
