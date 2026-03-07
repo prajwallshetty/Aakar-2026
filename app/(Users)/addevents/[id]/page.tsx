de304019d307080ac010d7543b79652e23ee440a
prajwallshetty <prajwal0shetty11@gmail.com>
2026-03-07 18:41:04 +0530

    fix:event data load

--- app/(Users)/addevents/[id]/page.tsx
+++ app/(Users)/addevents/[id]/page.tsx
@@ -99,15 +99,12 @@ export default function AddAdditionalEvents({
 
                 const eventsToFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
                 setFilteredEventIds(eventsToFilter);
-
                 const filteredOptions = eventOpts.map((category) => ({
                     label: category.label,
                     options: category.options.filter(
-                        (option) =>
-                            !eventsToFilter.includes(option.id) &&
-                            !userEvents?.some((ex) => ex.id === option.id)
-                    )
-                }));
+                        (option) => !userEvents?.some((ex) => ex.id === option.id)
+                 )
+            }));
 
                 setEventOptions(filteredOptions);
             } catch (error) {
