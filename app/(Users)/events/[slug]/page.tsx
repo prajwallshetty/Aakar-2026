06d43424c54ddd3a57787b1278a13890d7b8032b
prajwallshetty <prajwal0shetty11@gmail.com>
2026-03-17 06:11:38 +0530

    feat: Implement event category page with interactive 3D cards and add event slug generation utility.

--- app/(Users)/events/[slug]/page.tsx
+++ app/(Users)/events/[slug]/page.tsx
@@ -1,10 +1,12 @@
 import { getEventById } from "@/backend/events";
 import Eventdescription from "@/components/(User)/events/desc/eventdescription";
 import React from "react";
+import { extractIdFromSlug } from "@/lib/utils";
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const { id } = await params;
-    const eventData = await getEventById(parseInt(id));
+const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+    const { slug } = await params;
+    const id = extractIdFromSlug(slug);
+    const eventData = await getEventById(id);
 
     return (
         <Eventdescription eventData={eventData}/>
