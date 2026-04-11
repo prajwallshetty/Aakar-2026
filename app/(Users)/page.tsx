7a6f9ab97b51548013c189cc5c3001cd2424f90e
Prajwal Shetty <146322010+prajwallshetty@users.noreply.github.com>
2026-04-11 13:22:56 +0530

    Refactor Home component to remove enter screen
    Removed enter screen logic and related state management.

--- app/(Users)/page.tsx
+++ app/(Users)/page.tsx
@@ -1,37 +1,13 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import LandingPage from "@/components/(User)/Home/landingPage";
 import Eventcards from "@/components/(User)/Home/Eventcards";
 import MarqueeStrip from "@/components/(User)/Home/MarqueeStrip";
+import FeaturedActs from "@/components/(User)/Home/FeaturedActs";
 import MysteryReveal from "@/components/(User)/Home/MysteryReveal";
 import ElitePassPromo from "@/components/(User)/Home/ElitePassPromo";
 import CountdownRegister from "@/components/(User)/Home/CountdownRegister";
 import EnterScreen from "@/components/(User)/EnterScreen";
 
 export default function Home() {
-  const [showEnterScreen, setShowEnterScreen] = useState(true);
-
-  useEffect(() => {
-    const hasEntered = sessionStorage.getItem("hasEnteredSite");
-    if (hasEntered) {
-      setShowEnterScreen(false);
-    }
-  }, []);
-
-  if (showEnterScreen) {
-    return (
-      <div className="fixed inset-0 z-[9999] overflow-hidden bg-black">
-        <EnterScreen 
-          onEnter={() => {
-            sessionStorage.setItem("hasEnteredSite", "true");
-            setShowEnterScreen(false);
-          }} 
-        />
-      </div>
-    );
-  }
-
   return (
     <>
       <LandingPage />
