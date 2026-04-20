a7aceb362fe1a8afc708ae44bcaa163290d1e037
prajwallshetty <prajwal0shetty11@gmail.com>
2026-04-20 18:19:48 +0530

    fix:layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     "Experience the spirit of innovation, creativity, and competition at AAKAR 2026 – a premier TechnoCultural fest bringing together students, tech enthusiasts, and innovators for exciting events, workshops, and unforgettable memories.",
   keywords: [
     "AAKAR 2026",
-    "TechnoCultural Fest",
+    "TechnoCultural Fest 2026",
     "Engineering Fest",
     "Cultural Fest India",
     "Student Innovation",
